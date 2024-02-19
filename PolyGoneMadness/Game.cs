9f644d93835eb31542ca7c86e3d7fe4c07879608
Gunnar Pietz <gpietz@posteo.de>
2024-02-19 07:02:32 +0100

    Initial project checkin

---
+++ PolyGoneMadness/Game.cs
@@ -0,0 +1,53 @@
+﻿
+using OpenTK.Graphics.ES20;
+using OpenTK.Mathematics;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
+namespace PolyGoneMadness;
+
+public interface IGameWindow
+{
+
+}
+
+public sealed class Game : GameWindow, IGameWindow
+{
+    private int _screenWidth;
+    private int _screenHeight;
+    
+    public Game(int width, int height, string title) :base(GameWindowSettings.Default, NativeWindowSettings.Default)
+    {
+        _screenWidth = width;
+        _screenHeight = height;
+
+        CenterWindow(new Vector2i(_screenWidth, _screenHeight));
+    }
+
+    protected override void OnResize(ResizeEventArgs e)
+    {
+        base.OnResize(e);
+        GL.Viewport(0, 0, e.Width, e.Height);
+        _screenWidth = e.Width;
+        _screenHeight = e.Height;
+    }
+
+    protected override void OnUpdateFrame(FrameEventArgs args)
+    {
+        base.OnUpdateFrame(args);
+
+        if (KeyboardState.IsKeyDown(Keys.Escape))
+        {
+            Close();
+        }
+    }
+    
+    protected override void OnRenderFrame(FrameEventArgs args)
+    {
+        GL.ClearColor(0.1f, 0.1f, 0.25f, 1f);
+        GL.Clear(ClearBufferMask.ColorBufferBit);
+        Context.SwapBuffers();
+        base.OnRenderFrame(args);
+    }
+}
---
+++ PolyGoneMadness/Program.cs
@@ -0,0 +1,4 @@
+﻿using PolyGoneMadness;
+
+using var game = new Game(1024,768,"PolyGoneMadness");
+game.Run();
